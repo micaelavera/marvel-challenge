27929d4198261e3ff1d76f318827778fdae2be0d
Micaela Vera <micavera2010@hotmail.com>
2021-06-03 12:23:52 -0300

    added changes in navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,15 @@
 import React from "react";
 import './Navbar.css';
-// import logo from './images/logo-marvel.png';
+import logo from '../images/logo-marvel.png';
 
 const Navbar = () => {
     return (
         <div className="navbar">
-            {/* <img src={logo}/>  */}
-            <input type="text" class="fas" placeholder="&#xf002; Search"/>
-            <i class="fas fa-star"></i>
+            <a class="navbar-brand" href="#">
+                <img src={logo} alt="logo"/>
+            </a>
+            <input type="search" class="fas" placeholder="&#xf002; Search"/>
+            <i class="star far fa-star"></i>
         </div>
     );
 }
