4d85b979805308af9089dc3b03cdae2b00a8b1b6
Micaela Vera <micavera2010@hotmail.com>
2021-06-15 11:35:01 -0300

    added footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.css';
+
+const Footer = () =>{
+    return (
+        <div className="footer">
+            <p>&copy; Marvel 2021 | Micaela Vera</p>
+        </div>
+    );
+}
+export default Footer;
