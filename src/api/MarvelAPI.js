015604ecf68d37aec300aec3fd183495f252051e
Micaela Vera <micavera2010@hotmail.com>
2021-07-15 18:15:35 -0300

    add call to API to search for text similarities

--- src/api/MarvelAPI.js
+++ src/api/MarvelAPI.js
@@ -2,9 +2,10 @@ const baseURL = `https://gateway.marvel.com:443/v1/public/`;
 const publicKey = `8185dde56b63c7a4bc55982a91657ecc`;
 const hash = `036006dbbafb76de3387ba2d161ecb7f`;
 
-export const getCharacters = async () => {
+export const getCharacters = async (name) => {
     try {
-        const response = await fetch(`${baseURL}/characters?ts=1&apikey=${publicKey}&hash=${hash}`);
+        const response = await fetch(getFetch(name));
+        // const response = await fetch(`${baseURL}/characters?ts=1&apikey=${publicKey}&hash=${hash}`);
         const json = await response.json();
         const characters = json.data.results;
         return characters;   
@@ -12,6 +13,15 @@ export const getCharacters = async () => {
         console.log(error);
     }
 }
+
+export const getFetch = (name) => {
+    return (
+       name ? 
+       `${baseURL}/characters?nameStartsWith=${name}&ts=1&apikey=${publicKey}&hash=${hash}`
+       :
+       `${baseURL}/characters?ts=1&apikey=${publicKey}&hash=${hash}`
+    );
+   }
 
 export const getRandomCharacter = async () => {
     try {
@@ -35,3 +45,6 @@ export const getComics = async(characterId) => {
         console.log(error);
     }
 }
+
+console.log(getCharacters());
+
